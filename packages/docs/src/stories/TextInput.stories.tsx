b708fc949e6f8109df51a1040b351feb7590615a
DEV01 VALIANT <vgdev@VG-DEV01>
2024-02-01 16:51:05 -0300

    fix: Add ref to TextInput and adjust styles

--- packages/docs/src/stories/TextInput.stories.tsx
+++ packages/docs/src/stories/TextInput.stories.tsx
@@ -35,5 +35,6 @@ export const Disabled: StoryObj<TextInputProps> = {
 export const WhithPrefix: StoryObj<TextInputProps> = {
   args: {
     prefix: "cal.com/",
+    placeholder: "your-username",
   },
 };
--- packages/react/src/components/TextInput/styles.ts
+++ packages/react/src/components/TextInput/styles.ts
@@ -2,12 +2,23 @@ import { styled } from "../../styles";
 
 export const TextInputContainer = styled("div", {
   backgroundColor: "$gray900",
-  padding: "$3 $4",
+
   borderRadius: "$sm",
   boxSizing: "border-box",
   border: "2px solid $gray900",
   display: "flex",
-  alignItems: "baseline",
+  alignItems: "center",
+
+  variants: {
+    size: {
+      sm: {
+        padding: "$2 $3",
+      },
+      md: {
+        padding: "$3 $4",
+      },
+    },
+  },
 
   "&:has(input:focus)": {
     borderColor: "$ignite300",
@@ -17,33 +28,37 @@ export const TextInputContainer = styled("div", {
     opacity: 0.5,
     cursor: "not-allowed",
   },
+
+  defaultVariants: {
+    size: "md",
+  },
 });
 
 export const Prefix = styled("span", {
-  fontFamily: '$default',
-  fontSize: '$sm',
-  color: '$gray400',
-  fontWeight: 'regular',
+  fontFamily: "$default",
+  fontSize: "$sm",
+  color: "$gray400",
+  fontWeight: "regular",
 });
 
 export const Input = styled("input", {
-  fontFamily: '$default',
-  fontSize: '$sm',
-  color: '$white',
-  fontWeight: 'regular',
-  background: 'transparent',
+  fontFamily: "$default",
+  fontSize: "$sm",
+  color: "$white",
+  fontWeight: "regular",
+  background: "transparent",
   border: 0,
-  width: '100%',
+  width: "100%",
 
-  '&:focus': {
+  "&:focus": {
     outline: 0,
   },
 
-  '&:disabled': {
-    cursor: 'not-allowed',
+  "&:disabled": {
+    cursor: "not-allowed",
   },
 
-  '&:placeholder': {
-    color: '$gray400',
+  "&::placeholder": {
+    color: "$gray400",
   },
 });
